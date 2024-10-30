1baa9ad52afec873df17ef1f83198dcd97988915
bavly <bavly.remon2004@aucegypt.edu>
2024-10-30 23:38:31 +0300

    added changes to support image and encoding

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@
 // here we configure tonic
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/hello.proto")?;
+    tonic_build::compile_protos("proto/image_encoding.proto")?;
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,41 +1,61 @@
-use tonic::{ transport::Server, Request, Response, Status };
+use tonic::{transport::Server, Request, Response, Status};
+use image_encoding::image_encoder_server::{ImageEncoder, ImageEncoderServer};
+use image_encoding::{EncodedImageRequest, EncodedImageResponse};
 
-use hello::hello_server::{ Hello, HelloServer };
-use hello::{ HelloRequest, HelloResponse };
+// Import your encode_image function
 
-//TODO: actually have an image service through grpc
-pub mod hello {
-  tonic::include_proto!("hello");
+mod image_encoder;
+use rpc_service::image_encoder::encode_image;
+
+// This module is generated from your .proto file
+pub mod image_encoding {
+    tonic::include_proto!("image_encoding");
 }
 
-struct HelloService {}
+// Define your ImageEncoderService
+struct ImageEncoderService {}
 
 #[tonic::async_trait]
-impl Hello for HelloService {
-  async fn send_hello(
-    &self,
-    request: Request<HelloRequest>
-  ) -> Result<Response<HelloResponse>, Status> {
-    let request = request.into_inner();
-    println!("Got a request: {:?}", request); 
+impl ImageEncoder for ImageEncoderService {
+    async fn image_encode(
+        &self,
+        request: Request<EncodedImageRequest>,
+    ) -> Result<Response<EncodedImageResponse>, Status> {
+        let request = request.into_inner();
+        println!("Got a request: {:?}", request);
 
-    //image encode 
-    //then send in reply
+        // Get the image data from the request
+        let image_data = &request.image_data; // Assuming image_data is passed as bytes
 
-    let reply = hello::HelloResponse {
-      message: format!("Acknowledged:: {}!", request.name),
-    };
+        // Call the encode_image function with the provided image data
+        let encoded_image = match encode_image(image_data) { // Adjusted to use the image_data directly
+            Ok(encoded_data) => encoded_data,
+            Err(e) => {
+                eprintln!("Error encoding image: {}", e);
+                return Err(Status::internal("Image encoding failed"));
+            },
+        };
 
-    Ok(Response::new(reply))
-  }
+        // Construct the response with the encoded image data
+        let reply = EncodedImageResponse {
+            width: request.width,
+            height: request.height,
+            image_data: encoded_image,
+        };
+
+        Ok(Response::new(reply))
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let addr = "[::1]:50051".parse()?;
-  let hello_service = HelloService {};
+    let addr = "[::1]:50051".parse()?;
+    let image_encoder_service = ImageEncoderService {};
 
-  Server::builder().add_service(HelloServer::new(hello_service)).serve(addr).await?;
+    Server::builder()
+        .add_service(ImageEncoderServer::new(image_encoder_service))
+        .serve(addr)
+        .await?;
 
-  Ok(())
+    Ok(())
 }
