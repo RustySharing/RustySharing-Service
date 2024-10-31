0ab51d36036dbe4abc2cb5d76e9f7f4f18dda58d
Ahmed-Waseem77 <ahmedwaseemr77@gmail.com>
2024-11-01 01:42:49 +0200

    fix

--- src/image_encoder.rs
+++ src/image_encoder.rs
@@ -1,4 +1,5 @@
-use image::{ self, imageops, DynamicImage };
+use image::{ self, imageops, DynamicImage, GenericImageView };
+
 use serde::{ Serialize, Deserialize };
 use steganography::encoder;
 use std::error::Error;
@@ -13,6 +14,8 @@ struct EmbeddedData {
 // Function to encode an image with embedded data
 pub fn encode_image(img: DynamicImage) -> Result<Vec<u8>, Box<dyn Error>> {
   let blurred_img = imageops::blur(&img, 20.0);
+  let (width, height) = blurred_img.dimensions();
+  let low_res_img = imageops::resize(&blurred_img, width*13, height *13, image::imageops::FilterType::Lanczos3);
 
   // Prepare JSON data to embed
   let data = EmbeddedData {
@@ -31,12 +34,12 @@ pub fn encode_image(img: DynamicImage) -> Result<Vec<u8>, Box<dyn Error>> {
   let combined_data: Vec<u8> = [json_data.clone(), img_in_bytes].concat();
 
   // Check if combined data length exceeds image data capacity
-  if combined_data.len() > img_in_bytes_len {
-    return Err("Combined data length exceeds image data capacity".into());
-  }
+  // if combined_data.len() > img_in_bytes_len {
+  //   return Err("Combined data length exceeds image data capacity".into());
+  // }
 
   // Step 3: Encode JSON data into the image
-  let my_encoder = encoder::Encoder::new(&combined_data, image::ImageRgba8(blurred_img.clone()));
+  let my_encoder = encoder::Encoder::new(&combined_data, image::ImageRgba8(low_res_img.clone()));
   let encoded_img = my_encoder.encode_image();
 
   // Return the encoded image data if successful
--- src/server.rs
+++ src/server.rs
@@ -2,6 +2,8 @@ use tonic::{transport::Server, Request, Response, Status};
 use image_encoding::image_encoder_server::{ImageEncoder, ImageEncoderServer};
 use image_encoding::{EncodedImageRequest, EncodedImageResponse};
 
+use local_ip;
+
 // Import your encode_image function
 use rpc_service::image_encoder::encode_image;
 
@@ -25,11 +27,11 @@ impl ImageEncoder for ImageEncoderService {
         // Get the image data from the request
         let image_data = &request.image_data; // Assuming image_data is passed as bytes
         
+        // Step 1: Load the image from the byte data
+        let img = image::load_from_memory(&image_data)
+            .map_err(|_| Status::internal("Failed to load image"))?;
 
-        // image_data to DynamicImage
-        let img = image::load_from_memory(image_data).expect("Failed to load image");
-
-        // Call the encode_image function with the provided image data
+        //Call the encode_image function with the provided image data
         let encoded_image = match encode_image(img) { // Dereference the reference to the byte slice
             Ok(encoded_data) => encoded_data,
             Err(e) => {
@@ -37,6 +39,14 @@ impl ImageEncoder for ImageEncoderService {
                 return Err(Status::internal("Image encoding failed"));
             },
         };
+
+        let encoded_photo = image::load_from_memory(&image_data)
+            .map_err(|_| Status::internal("Failed to load image"))?;
+
+        // Step 2: Save the image to a file
+        let output_path = "output_image.jpeg"; // Specify your output file name
+        encoded_photo.save(output_path)
+            .map_err(|_| Status::internal("Failed to save image"))?;
 
         // Construct the response with the encoded image data
         let reply = EncodedImageResponse {
@@ -51,7 +61,8 @@ impl ImageEncoder for ImageEncoderService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
+  let ip = local_ip::get().unwrap();
+  let addr = format!("{}:50051", ip.to_string()).parse()?;
     let image_encoder_service = ImageEncoderService {};
 
     Server::builder()
