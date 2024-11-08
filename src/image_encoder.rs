a968f32552730d5329413e4a9b69fd98cac2f578
omarelfouly <omarelfouly@aucegypt.edu>
2024-11-09 00:01:06 +0200

    UUID file and del after

--- src/image_encoder.rs
+++ src/image_encoder.rs
@@ -4,75 +4,22 @@ use std::path::Path;
 use std::str;
 use stegano_core::SteganoCore;
 use steganography::util::bytes_to_file;
-// use stegano_core::commands::unveil;
-// use std::fs;
+use uuid::Uuid;
 
-// fn create_directory_if_not_exists(dir_path: &str) -> std::io::Result<()> {
-//     // Convert the dir_path to a Path
-//     let path = Path::new(dir_path);
-
-//     // Create the directory (and any parent directories) if it doesn't exist
-//     fs::create_dir_all(path)?;
-
-//     println!("Directory '{}' created or already exists.", dir_path);
-
-//     Ok(())
-// }
-
-// fn calculate_image_dimensions_from_data(
-//     data_size: usize,
-//     bits_per_pixel: u32,
-//     aspect_ratio: (u32, u32),
-// ) -> (u32, u32) {
-//     // Calculate the total number of bits needed
-//     let total_bits_needed = data_size * 8; // Convert bytes to bits
-
-//     // Calculate the number of pixels needed
-//     let pixels_needed = (total_bits_needed + bits_per_pixel as usize - 1) / bits_per_pixel as usize; // Round up
-
-//     // Aspect ratio
-//     let (aspect_width, aspect_height) = aspect_ratio;
-
-//     // Calculate the total aspect ratio
-//     let total_aspect = aspect_width as f32 / aspect_height as f32;
-
-//     // Calculate width and height based on the aspect ratio
-//     let height = (pixels_needed as f32 / total_aspect.sqrt()).sqrt().round() as u32;
-//     let width = (height as f32 * total_aspect).round() as u32;
-
-//     (width, height)
-// }
-
-// fn calculate_aspect_ratio(width: u32, height: u32) -> (u32, u32) {
-//     // Calculate the greatest common divisor (GCD) using the Euclidean algorithm
-//     fn gcd(a: u32, b: u32) -> u32 {
-//         if b == 0 {
-//             a
-//         } else {
-//             gcd(b, a % b)
-//         }
-//     }
-
-//     let divisor = gcd(width, height);
-//     (width / divisor, height / divisor)
-// }
-
-// Function to encode an image with embedded data
-pub fn encode_image(img: Vec<u8>) -> Result<String, Box<dyn Error>> {
-    print!("Started Encoding!");
+pub fn encode_image(img: Vec<u8>, file_name: &str) -> Result<String, Box<dyn Error>> {
+    print!("Started Encoding ... ");
 
     // let data = "Hello, steganography!\n2024-10-27T00:00:00Z".to_string();
-
-    let file = File::create("input.jpeg")?;
+    let img_path = file_name; // File to embed
+    let file = File::create(img_path)?;
 
     bytes_to_file(&img, &file);
-
-    let img_path = "input.jpeg"; // File to embed
 
     // let text_path = "input.txt";
     let input_paths: Vec<&str> = [img_path /*, text_path */].to_vec();
     let media_path = "carrier.png"; // Stego image (carrier)
-    let output_path = "image-with-a-file-inside.png"; // Output image with hidden file
+    let my_uuid = Uuid::now_v6(&[1, 2, 3, 4, 5, 6]);
+    let output_path = &format!("./encoded/{}.png", my_uuid); // Output image with hidden file
 
     // Ensure the source file (input.jpeg) and carrier image (double.png) exist
     if !Path::new(img_path).exists() {
@@ -101,19 +48,5 @@ pub fn encode_image(img: Vec<u8>) -> Result<String, Box<dyn Error>> {
 
     println!("Image with embedded data saved to: {}", output_path);
 
-    // let extraction_path = "./extracted"; // Path to save extracted image
-    // if let Err(e) = create_directory_if_not_exists(extraction_path) {
-    //     eprintln!("Error creating directory: {}", e);
-    // }
-    // // Extract the hidden file from the image
-    // let _ = unveil(
-    //     Path::new(output_path),
-    //     Path::new(extraction_path),
-    //     &CodecOptions::default(),
-    // );
-
-    // println!("Extracted file saved to: {}", extraction_path);
-
-    // Return the encoded image data if successful
     Ok(output_path.to_string())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,22 +1,22 @@
-use image_encoding::image_encoder_server::{ ImageEncoder, ImageEncoderServer };
-use image_encoding::{ EncodedImageRequest, EncodedImageResponse };
-use serde::{ Deserialize, Serialize };
+use image_encoding::image_encoder_server::{ImageEncoder, ImageEncoderServer};
+use image_encoding::{EncodedImageRequest, EncodedImageResponse};
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::str;
 use steganography::util::file_to_bytes;
-use tonic::{ transport::Server, Request, Response, Status };
+use tonic::{transport::Server, Request, Response, Status};
 // Import your encode_image function
 use rpc_service::image_encoder::encode_image;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct EmbeddedData {
-  message: String,
-  timestamp: String,
+    message: String,
+    timestamp: String,
 }
 
 // This module is generated from your .proto file
 pub mod image_encoding {
-  tonic::include_proto!("image_encoding");
+    tonic::include_proto!("image_encoding");
 }
 
 // Define your ImageEncoderService
@@ -24,52 +24,57 @@ struct ImageEncoderService {}
 
 #[tonic::async_trait]
 impl ImageEncoder for ImageEncoderService {
-  async fn image_encode(
-    &self,
-    request: Request<EncodedImageRequest>
-  ) -> Result<Response<EncodedImageResponse>, Status> {
-    let request = request.into_inner();
-    println!("Got a request!");
+    async fn image_encode(
+        &self,
+        request: Request<EncodedImageRequest>,
+    ) -> Result<Response<EncodedImageResponse>, Status> {
+        let request = request.into_inner();
+        println!("Got a request!");
 
-    // Get the image data from the request
-    let image_data = &request.image_data; // Assuming image_data is passed as bytes
+        // Get the image data from the request
+        let image_data = &request.image_data; // Assuming image_data is passed as bytes
+        let image_name = &request.file_name;
 
-    // Step 1: Load the image from the byte data
-    // let img = image::load_from_memory(image_data)
-    //     .map_err(|_| Status::internal("Failed to load image from memory"))?;
+        // Step 1: Load the image from the byte data
+        // let img = image::load_from_memory(image_data)
+        //     .map_err(|_| Status::internal("Failed to load image from memory"))?;
 
-    // Call the encode_image function with the loaded image
-    let encoded_image = match encode_image(image_data.clone()) {
-      // Pass the loaded image directly
-      Ok(encoded_img_path) => encoded_img_path,
-      Err(e) => {
-        eprintln!("Error encoding image: {}", e);
-        return Err(Status::internal("Image encoding failed"));
-      }
-    };
+        // Call the encode_image function with the loaded image
+        let encoded_image = match encode_image(image_data.clone(), image_name) {
+            // Pass the loaded image directly
+            Ok(encoded_img_path) => encoded_img_path,
+            Err(e) => {
+                eprintln!("Error encoding image: {}", e);
+                return Err(Status::internal("Image encoding failed"));
+            }
+        };
 
-    let file = File::open(encoded_image)?;
-    let encoded_bytes = file_to_bytes(file);
-    // Construct the response with the encoded image data
-    let reply = EncodedImageResponse {
-      image_data: encoded_bytes.clone(), // Echo the original image data in the response
-    };
+        let file = File::open(encoded_image.clone())?;
+        let encoded_bytes = file_to_bytes(file);
+        // Construct the response with the encoded image data
+        let reply = EncodedImageResponse {
+            image_data: encoded_bytes.clone(), // Echo the original image data in the response
+        };
 
-    Ok(Response::new(reply))
-  }
+        // delete file file when done
+        std::fs::remove_file(encoded_image)?;
+
+        Ok(Response::new(reply))
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let ip = local_ip::get().unwrap();
-  let addr = format!("{}:50051", ip.to_string()).parse()?;
-  //let addr = "[::1]:50051".parse()?;
-  let image_encoder_service = ImageEncoderService {};
+    let ip = local_ip::get().unwrap();
+    let addr = format!("{}:50051", ip.to_string()).parse()?;
+    //let addr = "[::1]:50051".parse()?;
+    let image_encoder_service = ImageEncoderService {};
 
-  Server::builder()
-    .max_frame_size(Some(10 * 1024 * 1024)) // Set to 10 MB
-    .add_service(ImageEncoderServer::new(image_encoder_service))
-    .serve(addr).await?;
+    Server::builder()
+        .max_frame_size(Some(10 * 1024 * 1024)) // Set to 10 MB
+        .add_service(ImageEncoderServer::new(image_encoder_service))
+        .serve(addr)
+        .await?;
 
-  Ok(())
+    Ok(())
 }
